3fbc3bd9c05924c6f6d91b13ebcd46c829fea993
Saiyam Sakhuja <88037577+sakhujasaiyam@users.noreply.github.com>
2025-03-22 01:45:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.isIntersecting) {
+                (entry.isIntersecting) {
                     entry.target.classList.add('show');
                     observer.unobserve(entry.target);
                 }
